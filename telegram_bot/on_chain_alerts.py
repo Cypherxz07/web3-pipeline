864680a77435ba35b012e8a1db7c565706ef0e2b
Cypher <abubakarshamsudeen2007@gmail.com>
2026-04-29 00:46:49 +0100

    All alerts linked to their respective chain explorers

--- telegram_bot/on_chain_alerts.py
+++ telegram_bot/on_chain_alerts.py
@@ -2,18 +2,26 @@ import asyncio
 from config import TELEGRAM_BOT_TOKEN_2, TELEGRAM_CHAT_ID
 from telegram import Bot
 
+CHAIN_EXPLORERS = {
+    'ethereum': 'etherscan.io',
+    'polygon': 'polygonscan.com',
+    'arbitrum': 'arbiscan.io'
+}
+
 async def send_whale_alert(transfer, threshold):
     """Send whale transfer alert to Telegram"""
     bot = Bot(token=TELEGRAM_BOT_TOKEN_2)
+    chain = transfer.get('chain', 'ethereum')
+    explorer = CHAIN_EXPLORERS.get(chain, 'etherscan.io')
     
     message = f"""
-🐋 **WHALE ALERT**
+🐋 **WHALE ALERT** [{chain.upper()}]
 
 Token: {transfer['token_symbol']}
 Amount: ${transfer['amount_usd']:,.2f}
 From: `{transfer['from'][:6]}...{transfer['from'][-4:]}`
 To: `{transfer['to'][:6]}...{transfer['to'][-4:]}`
-Tx: https://etherscan.io/tx/{transfer['tx_hash']}
+Tx: https://{explorer}/tx/{transfer['tx_hash']}
 Block: {transfer['block']}
 """
     
